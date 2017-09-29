c63d954b966be17cd74bc97f3975a1c002a4973c
JozinSbazin <rhanza@bk.ru>
2017-09-29 11:03:00 +0300

    *init commit (v1.0)

--- KillRevit/Program.cs
+++ KillRevit/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace KillRevit
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            try
+            {
+                var processes = Process.GetProcessesByName("Revit");
+                if (processes.Length == 0)
+                {
+                    Console.WriteLine("Процессов с именем \"Revit\" - не найдено!");
+                }
+                foreach (var process in processes)
+                {
+                    try
+                    {
+                        process.Kill();
+                        Console.WriteLine($"{process.ProcessName} ({process.Id}) - was killed!");
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Warning! {process.ProcessName} ({process.Id}) - kill attempt failed!");
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Для завершения нажмите любую клавишу");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла ошибка: \n{ex.Message}", "Ошибка", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
     }
 }
